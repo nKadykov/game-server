1cedd0642098293593db80cbba53d0a455f773e9
nKadykov <nkadykov@gmail.com>
2024-05-04 17:53:42 +0300

    init

--- game-server/network_server.cpp
+++ game-server/network_server.cpp
@@ -89,71 +89,71 @@ sf::Socket::Status NetworkServer::receiveClientData() {
 
 sf::Socket::Status NetworkServer::sendClientData()
 {
-	if (m_registration_step == 2) {
-		if (m_client_vector.size() > 1) {
-			for (int i = 0; i < m_client_vector.size() - 1; i++) {
-				if (!m_client_vector[i].done) {
-					if (m_client_vector[i].p_data_socket->isBlocking()) {
-						m_client_vector[i].p_data_socket->setBlocking(false);
-					}
-					sf::IpAddress temp_ip = m_client_vector[i].ip;
-					unsigned short tempPort = m_client_vector[i].port;
+	if (m_registration_step != 2) {
+		return sf::Socket::Status::Error;
+	}
+	if (m_client_vector.size() <= 1) {
+		m_registration_step = 3;
+		return sf::Socket::Status::Done;
+	}
+	for (int i = 0; i < m_client_vector.size() - 1; i++) {
+		if (!m_client_vector[i].done) {
+			if (m_client_vector[i].p_data_socket->isBlocking()) {
+				m_client_vector[i].p_data_socket->setBlocking(false);
+			}
+			sf::IpAddress temp_ip = m_client_vector[i].ip;
+			unsigned short temp_port = m_client_vector[i].port;
 
-					if (m_client_vector[i].send_packet.getDataSize() == 0)
-						m_client_vector[i].send_packet << "NEW" << m_client_vector.back().name;
+			if (m_client_vector[i].send_packet.getDataSize() == 0)
+				m_client_vector[i].send_packet << "NEW" << m_client_vector.back().name;
 
-					if (m_client_vector[i].p_data_socket->send(m_client_vector[i].send_packet, temp_ip, tempPort) == sf::Socket::Status::Done)
-					{
-						m_client_vector[i].done = true;
+			if (m_client_vector[i].p_data_socket->send(m_client_vector[i].send_packet, temp_ip, temp_port) == sf::Socket::Status::Done)
+			{
+				m_client_vector[i].done = true;
 
-						bool allIsDone = true;
-						for (int k = 0; k < m_client_vector.size() - 1; k++)
-						{
-							if (!m_client_vector[k].done) allIsDone = false;
-						}
-						if (allIsDone)
-						{
-							for (int j = 0; j < m_client_vector.size(); j++)
-							{
-								m_client_vector[j].send_packet.clear();
-								m_client_vector[j].done = false;
-							}
-							m_registration_step = 3;
-							return sf::Socket::Status::Done;
-						}
+				bool all_done = true;
+				for (int k = 0; k < m_client_vector.size() - 1; k++)
+				{
+					if (!m_client_vector[k].done) {
+						all_done = false;
 					}
 				}
+				if (all_done)
+				{
+					for (int j = 0; j < m_client_vector.size(); j++)
+					{
+						m_client_vector[j].send_packet.clear();
+						m_client_vector[j].done = false;
+					}
+					m_registration_step = 3;
+					return sf::Socket::Status::Done;
+				}
 			}
-			return sf::Socket::Status::NotReady;
-		}
-		else {
-			m_registration_step = 3;
-			return sf::Socket::Status::Done;
 		}
 	}
+	return sf::Socket::Status::NotReady;
 }
 
 sf::Socket::Status NetworkServer::sendDataPort()
 {
-	if (m_registration_step == 3)
-	{
-		if (m_registration_socket.isBlocking()) {
-			m_registration_socket.setBlocking(false);
-		}
+	if (m_registration_step != 3) {
+		return sf::Socket::Status::Error;
+	}
+	if (m_registration_socket.isBlocking()) {
+		m_registration_socket.setBlocking(false);
+	}
+	if (m_packet.getDataSize() == 0) {
+		m_packet << static_cast<sf::Uint16>(m_client_vector.back().p_data_socket->getLocalPort());
+	}
 
-		if (m_packet.getDataSize() == 0) {
-			m_packet << static_cast<sf::Uint16>(m_client_vector.back().p_data_socket->getLocalPort());
-		}
-
-		if (m_registration_socket.send(m_packet) == sf::Socket::Status::Done) {
-			std::cout << "sendDedicatedDataPort(): Dedicated data port sent\n";
-			m_registration_step = 4;
-			m_packet.clear();
-			return sf::Socket::Status::Done;
-		}
-		else {
-			return sf::Socket::Status::NotReady;
-		}
+	if (m_registration_socket.send(m_packet) == sf::Socket::Status::Done) {
+		std::cout << "sendDedicatedDataPort(): Dedicated data port sent\n";
+		m_registration_step = 4;
+		m_packet.clear();
+		return sf::Socket::Status::Done;
+	}
+	else {
+		return sf::Socket::Status::NotReady;
 	}
 }
 
@@ -189,9 +189,9 @@ sf::Socket::Status NetworkServer::receiveData(unsigned int& client_index)
 	for (int i = 0; i < m_client_vector.size(); i++) {
 		if (m_client_vector[i].p_data_socket->isBlocking()) m_client_vector[i].p_data_socket->setBlocking(false);
 		sf::IpAddress temp_ip = m_client_vector[i].ip;
-		unsigned short tempPort = m_client_vector[i].port;
+		unsigned short temp_port = m_client_vector[i].port;
 
-		if (m_client_vector[i].p_data_socket->receive(m_client_vector[i].registration_packet, temp_ip, tempPort) == sf::Socket::Status::Done) {
+		if (m_client_vector[i].p_data_socket->receive(m_client_vector[i].registration_packet, temp_ip, temp_port) == sf::Socket::Status::Done) {
 			client_index = i;
 
 			return sf::Socket::Status::Done;
@@ -210,23 +210,23 @@ sf::Socket::Status NetworkServer::sendData(sf::Packet data_packet)
 					m_client_vector[i].p_data_socket->setBlocking(false);
 				}
 				sf::IpAddress temp_ip = m_client_vector[i].ip;
-				unsigned short tempPort = m_client_vector[i].port;
+				unsigned short temp_port = m_client_vector[i].port;
 
 				if (m_client_vector[i].send_packet.getDataSize() == 0) {
 					m_client_vector[i].send_packet = data_packet;
 				}
 
-				if (m_client_vector[i].p_data_socket->send(m_client_vector[i].send_packet, temp_ip, tempPort) == sf::Socket::Status::Done) {
+				if (m_client_vector[i].p_data_socket->send(m_client_vector[i].send_packet, temp_ip, temp_port) == sf::Socket::Status::Done) {
 					m_client_vector[i].done = true;
 				}
 
-				bool allIsDone = true;
+				bool all_done = true;
 
 				for (int j = 0; j < m_client_vector.size(); j++) {
-					if (m_client_vector[j].done == false) allIsDone = false;
+					if (m_client_vector[j].done == false) all_done = false;
 				}
 
-				if (allIsDone) {
+				if (all_done) {
 					for (int j = 0; j < m_client_vector.size(); j++) {
 						m_client_vector[j].send_packet.clear();
 						m_client_vector[j].done = false;
